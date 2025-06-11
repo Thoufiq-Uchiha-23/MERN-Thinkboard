e06a3a429c42737c6b97429d7e1ebd75ce8b863c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-06-11 22:52:00 +0530

    Basic setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+// What is Endpoint?
+// An endpoint is a combination of URL + HTTP method that lets the user(aka client) interact with a specific resource
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("You got 21 notes")
+})
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message: "Note created successfully!"})
+})
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note updated successfully!"})
+})
+
+app.delete("/api/notes", (req, res) => {
+    res.status(200).json({message: "Note deleted successfully!"})
+})
+
+app.listen(5001, () => {
+    console.log("Server started on PORT : 5001")
+})

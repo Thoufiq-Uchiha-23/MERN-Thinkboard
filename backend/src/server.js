09e14d93a0bb84ca531e264ec6196c9cc7c851f9
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-06-16 23:07:45 +0530

    updated middlewares

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/notes", notesRoutes);
 

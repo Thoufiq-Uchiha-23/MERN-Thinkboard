134452dfd3eaae0ad84e7e15a2a7965629438fdd
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-06-21 10:36:01 +0530

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,10 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
+    <div data>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

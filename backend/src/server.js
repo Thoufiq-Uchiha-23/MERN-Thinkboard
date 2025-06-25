2de3690d308ebfbe86ba934f0e13dafa581dc2af
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-06-25 10:18:41 +0530

    backend for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,28 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 // middleware
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(rateLimiter);
@@ -26,6 +34,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
